033b30d062c35d96fb3fee015e69c97ece2d0785
Albert Choi <achoi@venacorp.com>
2022-02-15 12:52:33 -0500

    Optimization - remove redundant call
    processMetadata() already calls syncAnnotatedClasses() as the first thing it does

--- hibernate-core/src/main/java/org/hibernate/cfg/Configuration.java
+++ hibernate-core/src/main/java/org/hibernate/cfg/Configuration.java
@@ -1408,7 +1408,6 @@ public class Configuration implements Serializable {
 
 		// process metadata queue
 		{
-			metadataSourceQueue.syncAnnotatedClasses();
 			metadataSourceQueue.processMetadata( determineMetadataSourcePrecedence() );
 		}
 
